d9c1e2f05269b869f3541278156451d2dce2fcd5
Anton Orlichenko <aorliche@gmail.com>
2022-08-13 21:18:57 -0500

    Add dbtest

---
+++ test/dbtest.php
@@ -0,0 +1,22 @@
+<?php
+	require_once('../mysql.php');
+    
+    $stmt = $mysqli->prepare('select * from videos');
+    $stmt->execute();
+    $res = $stmt->get_result();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<?=ini_get('upload_max_filesize');?>
+<?php
+    while ($row = $res->fetch_assoc()) {
+?>
+    <div>Video: <?=$row['name']?> Id: <?=$row['id']?> User:<?=$row['userid']?> Uniqid: <?=$row['uniqid']?> </div>
+<?php        
+    }
+?>
+</body>
+</html>
