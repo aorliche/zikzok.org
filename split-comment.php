14b4598fb0d8920293fa9a8fbe8123f49f18b239
Anton Orlichenko <aorliche@gmail.com>
2023-03-29 06:28:14 -0500

    Fix

--- split-comment.php
+++ split-comment.php
@@ -5,7 +5,7 @@ $c = intval($_GET['c']);
 $v = $_GET['v'];
 $before = $_GET['before'];
 $after = $_GET['after'];
-$stmt = $mysqli->prepare('insert into comment_splits (split_which, uniqid, before, after) values (?, ?, ?, ?)');
+$stmt = $mysqli->prepare('insert into comment_splits (split_which, uniqid, `before`, `after`) values (?, ?, ?, ?)');
 $stmt->bind_param('isss', $c, $v, $before, $after);
 $stmt->execute();
 
