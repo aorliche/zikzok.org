c4c5687219c311b239ce25035dcf586c2fbc439b
Anton Orlichenko <aorliche@gmail.com>
2023-03-29 01:55:57 -0500

    Fix

--- video.php
+++ video.php
@@ -288,7 +288,7 @@ EOT;
             echo "<li><strong>$name</strong> on $created: $before... <ul><li><a href='/video.php?v=$middle'><img src='/preview/$middle.png' width='100'></a></li></ul> ...$after";
         } else {
             echo "<li><strong>$name</strong> on $created: <span id='comment-$cid'>$comment</span> <a class='split-link' href='#' data-video='$uniqid' data-comment='$cid'>Split</a>
-                <span class='split-word-prompt'>Select word to split on:</span> <input type='range' min='0' max='0' value='0' class='split-input'> <span class='split-word-feedback'></span> <select class='split-select'></li>";
+                <span class='split-word-prompt'>Select word to split on:</span> <input type='range' min='0' max='0' value='0' class='split-word-input'> <span class='split-word-feedback'></span> <select class='split-select'></select></li>";
         }
         /*if ($other) {
             $other = "[Split By:] ";
